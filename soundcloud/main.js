bdeb6b6f3a5e9639b85d0aae4a269635c2c57826
clarkmcadoo <clark.mcadoo@gmail.com>
2017-06-17 16:55:46 -0500

    improved search

--- soundcloud/main.js
+++ soundcloud/main.js
@@ -3,15 +3,78 @@
 */
 
 // 1. First select and store the elements you'll be working with
+var container = document.querySelector(".container");
+var header = document.querySelector("h1");
+var player = document.querySelector(".player");
+var musicPlayer = document.querySelector(".music-player");
+var search = document.querySelector(".search");
+var searchForm = document.querySelector(".search-form");
+var results = document.querySelector(".results");
+var searchBarInput = document.querySelector("#search-bar");
+var submit = document.querySelector(".submit");
+submit.addEventListener("click", createPageURL);
+console.log(searchBarInput.value);
+// submit = event.preventDefault();
 
+(function() {
+  // 2. Create your `onSubmit` event for getting the user's search term
+  function submitMessage() {
+    alert("Getting the User's search term");
+  }
+});
 
-// 2. Create your `onSubmit` event for getting the user's search term
+  // 3. Create your `fetch` request that is called after a submission
+function createPageURL(){
+  event.preventDefault();
+  var baseURL = "https://api.soundcloud.com/users/";
+  var searchResult = searchBarInput.value;
+  var client = "?client_id=8538a1744a7fdaa59981232897501e04";
+  var url = baseURL + searchResult + client;
 
+  
+console.log(url);
 
-// 3. Create your `fetch` request that is called after a submission
+  fetch(
+    url
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data.results);
+      var artistInfo = data.results;
+      for (let i = 0; i < 15; i++) {
+        createArtist(data[i]);
+      }
+    });
 
+  // 4. Create a way to append the fetch results to your page
+  var nowPlayingText = document.createElement("h5");
+  player.appendChild(nowPlayingText);
+  nowPlayingText.innerHTML = "Now Playing:" /* + */;
 
-// 4. Create a way to append the fetch results to your page
+  var searchResultsText = document.createElement("h3");
+  results.appendChild(searchResultsText);
+  searchResultsText.innerHTML = "Search Results:";
 
+  function createArtist(array) {
+    var artistProfile = document.createElement("div");
+    results.appendChild(artistProfile);
 
-// 5. Create a way to listen for a click that will play the song in the audio play
+    var artistJacket = document.createElement("img");
+    artistProfile.appendChild(artistJacket);
+    artistJacket.src = array.artwork_url;
+    artistJacket.id = "artist-img";
+
+    var artistTitle = document.createElement("p");
+    artistProfile.appendChild(artistTitle);
+    artistTitle.innerHTML = array.title;
+    artistTitle.id = "artist-title";
+
+    var artistName = document.createElement("h3");
+    artistProfile.appendChild(artistName);
+    artistName.innerHTML = array.user.username;
+    artistName.id = "artist-name";
+  }
+};
+  // 5. Create a way to listen for a click that will play the song in the audio play
