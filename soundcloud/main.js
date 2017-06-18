94818a99e626f45a993bf9f1254ef2ecdb5b55a7
clarkmcadoo <clark.mcadoo@gmail.com>
2017-06-18 10:39:55 -0500

    able to load tracks to page

--- soundcloud/main.js
+++ soundcloud/main.js
@@ -9,63 +9,50 @@ var player = document.querySelector(".player");
 var musicPlayer = document.querySelector(".music-player");
 var search = document.querySelector(".search");
 var searchForm = document.querySelector(".search-form");
+
 var results = document.querySelector(".results");
 var searchBarInput = document.querySelector("#search-bar");
 var submit = document.querySelector(".submit");
+
+// 2. Create your `onSubmit` event for getting the user's search term
+
 submit.addEventListener("click", createPageURL);
-console.log(searchBarInput.value);
-// submit = event.preventDefault();
 
-(function() {
-  // 2. Create your `onSubmit` event for getting the user's search term
-  function submitMessage() {
-    alert("Getting the User's search term");
-  }
-});
-
-  // 3. Create your `fetch` request that is called after a submission
-function createPageURL(){
+// 3. Create your `fetch` request that is called after a submission
+function createPageURL() {
   event.preventDefault();
   var baseURL = "https://api.soundcloud.com/users/";
   var userInput = searchBarInput.value;
-  var searchResult = userInput.replace(" ","+");
+  var searchResult = userInput.replace(" ", "+"); //need to correct this.
   var client = "?client_id=8538a1744a7fdaa59981232897501e04";
   var url1 = baseURL + searchResult + client;
+  console.log("user Input:", userInput);
+  console.log("url1", url1);
 
-fetch (url1).then(function(response1){
-  return response1.json();
-})
-.then(function(firstData){
-  console.log(firstData);
- searchedUserID = firstData.id;
- console.log(searchedUserID);
- return searchedUserID;
-});
-
-  var tracksUrl = baseURL + searchedUserID + "/tracks" + client;
-  
-  fetch(
-    tracksUrl
-  )
-    .then(function(response) {
-      return response.json();
+  fetch(url1)
+    .then(function(response1) {
+      return response1.json();
     })
-    .then(function(data) {
-      console.log(data.results);
-      var artistInfo = data.results;
-      for (let i = 0; i < 15; i++) {
-        createArtist(data[i]);
-      }
+    .then(function(firstData) {
+      searchedUserID = firstData.id;
+      findUserTracks(searchedUserID);
+      console.log(searchedUserID);
     });
 
-  // 4. Create a way to append the fetch results to your page
-  var nowPlayingText = document.createElement("h5");
-  player.appendChild(nowPlayingText);
-  nowPlayingText.innerHTML = "Now Playing:" /* + */;
+  function findUserTracks(userIDNum) {
+    var tracksUrl = baseURL + userIDNum + "/tracks" + client;
+    fetch(tracksUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        for (let i = 0; i < 15; i++) {
+          createArtist(data[i]);
+        }
+      });
+  }
 
-  var searchResultsText = document.createElement("h3");
-  results.appendChild(searchResultsText);
-  searchResultsText.innerHTML = "Search Results:";
+  // 4. Create a way to append the fetch results to your page
 
   function createArtist(array) {
     var artistProfile = document.createElement("div");
@@ -86,5 +73,26 @@ fetch (url1).then(function(response1){
     artistName.innerHTML = array.user.username;
     artistName.id = "artist-name";
   }
-};
-  // 5. Create a way to listen for a click that will play the song in the audio play
+}
+// 5. Create a way to listen for a click that will play the song in the audio play
+
+// var numbers = document.querySelectorAll(".results div");
+// for (i=0; i<15; i++){
+//     numbers[i].addEventListener("click", playMusic)
+// }
+// function playMusic(){
+//     var content = event.target.textContent;
+
+
+
+
+// }
+
+
+var nowPlayingText = document.createElement("h5");
+player.appendChild(nowPlayingText);
+nowPlayingText.innerHTML = "Now Playing:" /* + */;
+
+var searchResultsText = document.createElement("h3");
+results.appendChild(searchResultsText);
+searchResultsText.innerHTML = "Search Results:";
